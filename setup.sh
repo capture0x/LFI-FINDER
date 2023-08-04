1bad178cfae44b31b66fb07d2c901ee647799e4e
capture0x <61238348+capture0x@users.noreply.github.com>
2023-08-04 16:25:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/115.0.5790.170/linux64/chrome-linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
